c187e67457c4d91492627277d59c2b06af6caee4
kamil-acar <kamilfurkanacar@gmail.com>
2024-04-09 06:25:51 +0300

    Mp3-player

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Mp3-player
- MP3 player application I made with Html Css and Js
+
+<h2>MP3 player application I made with Html Css and Js</h2>
+
+![](m3-player.gif)
---
+++ main.js
@@ -0,0 +1,272 @@
+console.log("hello word")
+
+/* elementlere ulasip obje olarak kullanma, yakalama*/
+const prevButton = document.getElementById('prev')
+const nextButton = document.getElementById('next')
+const repeatButton = document.getElementById('repeat')
+const shuffleButton = document.getElementById('shuffle')
+const pauseButton = document.getElementById('pause')
+const playButton = document.getElementById('play')
+
+const audio = document.getElementById('audio')
+const songImage = document.getElementById('song-image')
+const songName = document.getElementById('song-name')
+const songArtist = document.getElementById('song-artist')
+
+const playListButton = document.getElementById('playlist')
+
+
+const maxDuration = document.getElementById('max-duration')
+const currentTimeRef = document.getElementById('current-time')
+
+const progressBar = document.getElementById('progress-bar')
+const playListContainer = document.getElementById('playlist-container')
+const closeButton= document.getElementById('close-button')
+const playListSongs = document.getElementById('playlist-songs')
+
+const currentProgress = document.getElementById('current-progress')
+
+
+
+
+//------Sira------
+let index
+
+
+console.log("hello word 1")
+
+
+
+//------Dongu------
+
+let loop=true
+
+//------json-sarkı-listesi------
+
+const songList=[
+    {
+        name: "Bring Me To Life",
+        link: "assets/Bring Me To Life.mp3",
+        artist: "Evanescence",
+        image: "assets/Evanescence.jpg"
+    },
+    {
+        name: "Fairytale",
+        link: "assets/Fairytale.mp3",
+        artist: "Alexander Rybak",
+        image: "assets/Alexander Rybak .jpg"
+    },
+    {
+        name: "Lights",
+        link: "assets/Lights.mp3",
+        artist: "Ellie Goulding",
+        image: "assets/Ellie Goulding.jpg"
+    },
+    {
+        name: "This I Love",
+        link: "assets/This I Love.mp3",
+        artist: "Guns N' Roses",
+        image: "assets/Guns N' Roses.jpg"
+    },
+    {
+        name: "Outside",
+        link: "assets/Outside.mp3",
+        artist: "Calvin Harris",
+        image: "assets/Calvin Harris.jpg"
+    }
+]
+
+// time formatter
+const timeFormatter =(timeInput)=>{
+    let minute = Math.floor(timeInput / 60)
+    minute = minute <10 ? "0"+ minute : minute
+    let second=Math.floor(timeInput % 60)
+    second= second <10 ? "0" + second : second
+    return `${minute}:${second}`
+}
+
+//------Sarki-Atama------
+
+console.log("hello word 2")
+const setSong=(arrayIndex) => {
+    console.log(arrayIndex)    
+    let {name,link,artist,image} = songList[arrayIndex]
+    
+    // Audio atanacak
+    audio.src=link
+    songName.innerHTML=name
+    songArtist.innerHTML=artist
+    songImage.src=image
+
+    audio.onloadeddata=()=>{
+        maxDuration.innerText=timeFormatter(audio.duration)
+        // En fazla valti ver max duraction
+    }
+    // container eger görünmüyorsa yok et
+    playListContainer.classList.add("hide")
+    playAudio()
+
+
+}
+
+const playAudio=()=>{
+    audio.play()
+    pauseButton.classList.remove("hide")
+    playButton.classList.add("hide")
+}
+
+const pauseAudio=()=>{
+    audio.pause()
+    pauseButton.classList.add("hide")
+    playButton.classList.remove("hide")
+}
+
+const nextSong=()=>{
+    if(loop){
+        if(index == (songList.length-1)){
+            index=0
+        }else{
+            index +=1
+        }
+        setSong(index)
+        playAudio()
+    }else{
+        let randIndex=Math.floor(Math.random() * songList.length)
+        console.log(randIndex)
+        setSong(randIndex)
+        playAudio()
+    }
+}
+
+const previousSong=()=>{
+    if(index>0){
+        pauseAudio()
+        index-=1
+    }else{
+        index=songList.length - 1
+    }
+    setSong(index)
+    playAudio()
+}
+console.log("hello word 3")
+
+
+audio.onended= ()=>{
+    nextSong()
+}
+
+
+
+
+
+progressBar.addEventListener("click",(event)=>{
+
+    let coordStart= progressBar.getBoundingClientRect().left
+    let coodEnd=event.clientX
+    let progress=(coodEnd-coordStart)/progressBar.offsetWidth
+
+    currentProgress.style.width=progress * 100 + "%"
+    audio.currentTime=progress * audio.duration
+
+    console.log(coordStart)
+    console.log(coodEnd)
+    console.log(progress)
+    console.log(audio.currentTime)
+
+    audio.play()
+    pauseButton.classList.remove("hide")
+    playButton.classList.add("hide")
+})
+
+// Sarki listesi
+
+const initializePlayList=()=>{
+    for (const i in songList) {
+        playListSongs.innerHTML +=`<li class="playlistSong"
+        onclick="setSong(${i})">
+        <div class="playlist-image-container">
+        <img src="${songList[i].image}"/>
+        </div>
+        <div class="playlist-song-details">
+        <span id="playlist-song-name">
+        ${songList[i].name}
+        </span>
+        <span id="playlist-song-artist-album">
+        ${songList[i].artist}
+        </span>
+        </div>
+        </li>`
+    }
+}
+
+playListButton.addEventListener("click",()=>{
+    playListContainer.classList.remove("hide")
+})
+
+closeButton.addEventListener("click",()=>{
+    playListContainer.classList.add("hide")
+})
+
+// play button
+
+playButton.addEventListener("click",playAudio)
+
+// pause button
+pauseButton.addEventListener("click",pauseAudio)
+
+// next Button
+nextButton.addEventListener("click",nextSong)
+
+// prev button
+prevButton.addEventListener("click",previousSong)
+
+// karıştır tiklandığında
+
+shuffleButton.addEventListener("click",()=>{
+    if(shuffleButton.classList.contains("active")){
+        shuffleButton.classList.remove("active")
+        loop=true
+        console.log("tekrar kapalı")
+    }else{
+        shuffleButton.classList.add("active")
+        loop=false
+        console.log("tekrar açık")
+    }
+})
+
+
+repeatButton.addEventListener("click",()=>{
+    if(repeatButton.classList.contains("active")){
+        repeatButton.classList.remove("active")
+        loop=false
+        console.log("tekrar kapalı")
+    }else{
+        repeatButton.classList.add("active")
+        loop=true
+        console.log("tekrar açık")
+    }
+})
+
+setInterval( ()=>{
+    currentTimeRef.innerHTML=timeFormatter(audio.currentTime)
+    currentProgress.style.width=(audio.currentTime/audio.duration.toFixed(3))*100 + "%"
+}, 1000);
+
+// zaman güncellemesi yakala
+audio.addEventListener("timeupdate",()=>{
+    currentTimeRef.innerText=timeFormatter(audio.currentTime)
+})
+
+// ekran yüklendıgınde
+window.onload=()=>{
+    index=0
+    setSong(index)
+    pauseAudio()
+    initializePlayList()
+
+    // oynatma listesinin ayarla
+}
+console.log("hello word 4")
+
+
+
